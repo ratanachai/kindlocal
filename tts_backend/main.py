6fd8b5fea2aed3da904c017fc8fe1ecb9b0f62d4
Ken <ratanachai@gmail.com>
2019-10-06 22:22:58 +0700

    update tts_backend/main.py

--- tts_backend/main.py
+++ tts_backend/main.py
@@ -5,3 +5,11 @@ from api.services import translate_tts_api
 app = Flask(__name__)
 
 app.register_blueprint(translate_tts_api)
+
+if __name__ == '__main__':
+    try:
+        port = int(sys.argv[1])
+    except Exception as e:
+        port = 5000
+
+    app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)
